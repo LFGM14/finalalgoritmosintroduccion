18602afff0a8966c2ff921202b8cf1db212c582d
LFGM14 <74067091+LFGM14@users.noreply.github.com>
2023-11-18 18:54:16 -0500

    banderas 2x3, antes de terminar movimiento

--- ConsoleApplication13/ConsoleApplication13.cpp
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -8,36 +8,43 @@
 #include "utilidades.h"
 #include "mapsAndRunners.h"
 #include "startAndEndScreens.h"
+#define FILAS 25
+#define COLUMNAS 80
 
 using namespace std;
 using namespace System;
 
-#define FILAS 25
-#define COLUMNAS 80
-#define rightMov 77
-#define leftMov 75
-#define upMov 72
-#define downMov 80
-#define jumpMov 32
-
 void gameStart() {
+	int yPosRunnersMap1[4] = { 8, 12, 16, 20 };
+	int xPosRunners = 0;
+	float dxRunnersMap1[4];
+	int dyRunnersMap1 = 0;
+	for (int i = 0; i < 4; i++) {
+		dxRunnersMap1[i] = rand() % 2;
+	}
+
 
 }
 
 int main() {
-    Console::SetWindowSize(80, 25);
-    srand(time(NULL));
+	Console::SetWindowSize(80, 25);
+	srand(time(NULL));
 
-    //dibujar_mapa(map1);
-    flag_SU(flagH, 10, 10);
-
-    system("pause>0");
-
-    /*drawStart(start);*/
-
-
+	//dibujar_mapa(map2);
+	//flag_PE(flagV);
+	//flag_MX(flagV);
+	//flag_AR(flagH);
+	//flag_BO(flagH);
+	//flag_BR(flagH);
+	//flag_CO(flagH);
+	//flag_NI(flagH);
+	//flag_PA(flagH);
+	flag_SU(flagH);
+	
+	system("pause>0");
 	return 0;
 }
+
 
 
 
